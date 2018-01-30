224dc95be8590d1769b3abb519cbc3d960e56cde
SirRonnoc <connor.schmehl@gmail.com>
2018-01-30 08:37:28 -0500

    initial main

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,37 @@
-public class Main {
+import java.awt.Graphics;
+
+import javax.swing.JComponent;
+import javax.swing.JFrame;
+
+public class Main extends JFrame{
+	
+	//declaration of initial variables
+	private int windowX, windowY;
+
+	/**
+	 * sets up the Main window
+	 * @param xSize - x size of the window as set by the user
+	 * @param ySize - y size of the window as set by the user
+	 */
+	public Main(int xSize, int ySize) {
+		//moving values passes by main menu down to the game window
+		this.windowX = xSize; this.windowY = ySize;
+		this.setSize(windowX,windowY);
+		this.setVisible(true);
+	}
+	
+	/**
+	 * this is the draw class. It extends JComponent and only has a paint method, which is called by the JFrame to draw graphics to the window.
+	 * @author 2019006
+	 *
+	 */
+	private class Draw extends JComponent {
+		public void paint(Graphics g) {
+			
+		}
+	}
+	// not permanent, here to test
 	public static void main(String[] args) {
-		System.out.println("hello world");
+		Main main = new Main(800,800);
 	}
 }
