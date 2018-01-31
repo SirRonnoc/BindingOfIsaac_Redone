dc03b1366f493a6d51a0846671f4763437090424
SirRonnoc <connor.schmehl@gmail.com>
2018-01-31 08:34:31 -0500

    entity update

--- src/Entities/Entity.java
+++ src/Entities/Entity.java
@@ -3,7 +3,24 @@ package Entities;
 import java.awt.image.BufferedImage;
 
 public class Entity {
+	protected int maxHealth;
 	protected int health;
 	protected int speed;
-	protected BufferedImage[] idleAnimations;
+	protected BufferedImage[] downAnimations;
+	protected BufferedImage[] upAnimations;
+	protected BufferedImage[] leftAnimations;
+	protected BufferedImage[] rightAnimations;
+	/**
+	 * instantiates the basic entity with variables
+	 * @param mH - max health of the entity
+	 * @param sp - speed of the entity
+	 * @param iA - idle animations of the entity
+	 */
+	public Entity(int mH, int sp, BufferedImage[] dA, BufferedImage[] uA, BufferedImage[] lA, BufferedImage[] rA) {
+		//sets initial variables
+		this.maxHealth = mH;
+		this.speed = sp;
+		this.downAnimations = dA; this.rightAnimations = rA; this.leftAnimations = lA; this.upAnimations = uA;
+		this.health = this.maxHealth;
+	}
 }
