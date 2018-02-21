0c62d205edb21c6b6f6ce7a0bda99c7dc9c13270
SirRonnoc <connor.schmehl@gmail.com>
2018-02-21 08:50:42 -0500

    not much, some enemy changes

--- src/Entities/Enemy.java
+++ src/Entities/Enemy.java
@@ -2,6 +2,7 @@ package Entities;
 
 public abstract class Enemy extends Entity{
 	protected int movDir;
+	protected int onHitDamage;
 	/**
 	 * initializes the enemy with values and setup information 
 	 * @param mH - max health of the enemy
@@ -11,11 +12,24 @@ public abstract class Enemy extends Entity{
 	 * @param yP - y position of the enemy
 	 * @param h - height of the enemy
 	 * @param w - width of the enemy
+	 * @param oHD - on hit damage of the enemy
 	 */
-	public Enemy(int mH, int sp, int aS, int xP, int yP, int h, int w) {
+	public Enemy(int mH, int sp, int aS, int xP, int yP, int h, int w, int oHD) {
 		super(mH,sp,aS,xP,yP,h,w);
+		this.onHitDamage = oHD;
 		
 	}
+	/**
+	 * unimplemented update for all classes of type Enemy
+	 */
 	public abstract void update();
+	
+	/**
+	 * returns the damage this enemy does on hit
+	 * @return - the on hit damage of the enemy
+	 */
+	public int getOnHitDamage() {
+		return this.onHitDamage;
+	}
 	
 }
