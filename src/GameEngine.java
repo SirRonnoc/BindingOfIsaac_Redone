6466cc8b2e0686aca156b34d9596b2c372e27c83
JamesDMar <2019032@appleby.on.ca>
2018-02-20 23:19:34 -0500

    Wow realized how absolute trash that last one was here is a half decent circle circle collision
    IT TAKES ANY 2 ENTITIES!!!

--- src/GameEngine.java
+++ src/GameEngine.java
@@ -5,12 +5,18 @@ public class GameEngine {
     }
 
     public static boolean checkCollision(Entity entity, Entity entity2){
-        if (((entity.getXPos()-(entity.getWidth()/2))<(entity2.getXPos()+(entity2.getWidth()/2)))&&((entity.getXPos()+(entity.getWidth()/2))>(entity2.getXPos()-(entity2.getWidth()/2)))){
-            if (((entity.getYPos()-(entity.getHeight()/2))<(entity2.getYPos()+(entity2.getHeight()/2)))&&((entity.getYPos()+(entity.getHeight()/2))>(entity2.getYPos()-(entity2.getHeight()/2)))){
-                System.out.println(true);
-                return true;
+        int x1 = entity.getXPos();
+        int y1 = entity.getYPos();
+        int x2 = entity2.getXPos();
+        int y2 = entity2.getYPos();
+        double r1 = entity.getWidth()/4;
+        double r2 = entity2.getWidth()/4;
+        double distance =((Math.abs(x1-x2)^2) + (Math.abs(y1-y2)^2));
 
-            }
+        if (distance<(r1+r2)){
+            System.out.println(distance);
+            System.out.println(r1+r2);
+            return true;
         }
         return false;
     }
