c7ed3df18a70e06fb69f2c5b381b3a2675291a0d
JamesDMar <2019032@appleby.on.ca>
2018-02-27 09:39:48 -0500

    here we go

--- src/Rooms/Room.java
+++ src/Rooms/Room.java
@@ -1,7 +1,9 @@
 package Rooms;
+import Entities.Enemy;
 import Tools.GameFileReader;
 import javax.print.DocFlavor;
 import java.awt.image.BufferedImage;
+import java.util.ArrayList;
 
 /**
  * Room class basis for all rooms.
@@ -9,10 +11,7 @@ import java.awt.image.BufferedImage;
 public class Room {
     protected int dimensionX, dimensionY;
     public static int pieceSize=156;
-    protected static String background;
-    protected static BufferedImage[] backgroundImages = new BufferedImage[6];
     protected boolean doorTop,doorRight,doorBot,doorLeft;
-    protected static BufferedImage doorImgTop,doorImgRight,doorImgBot,doorImgLeft;
     protected static int wallWidth=90;
     static int[] topDoorPos= new int[2];
     static int[] botDoorPos= new int[2];
@@ -31,16 +30,26 @@ public class Room {
 
 
     }
+
+    /**
+     * Room Constructor
+     * @param x
+     * @param y
+     */
     public Room(int x, int y){
         this.dimensionX=x;
         this.dimensionY=y;
     }
+
+    /**
+     * Called in the GameEngine start method loads in all variables that are common through all rooms
+     */
     public static void init(){
-        topDoorPos[0]= BasementRoom.getRoomImages()[0].getWidth()-getDoorImgTop().getWidth()/2;
+        topDoorPos[0]= BasementRoom.getRoomImages()[0].getWidth()-BasementRoom.getDoorImgTop().getWidth()/2;
         topDoorPos[1]=25;
         rightDoorPos[0]= BasementRoom.getRoomImages()[0].getWidth()*2-154;
         rightDoorPos[1]=BasementRoom.getRoomImages()[0].getHeight()-64;
-        botDoorPos[0]= BasementRoom.getRoomImages()[0].getWidth()-getDoorImgTop().getWidth()/2;
+        botDoorPos[0]= BasementRoom.getRoomImages()[0].getWidth()-BasementRoom.getDoorImgTop().getWidth()/2;
         botDoorPos[1]=BasementRoom.getRoomImages()[0].getHeight()*2-135;
         leftDoorPos[0]= 10;
         leftDoorPos[1]= BasementRoom.getRoomImages()[0].getHeight()-58;
@@ -48,31 +57,20 @@ public class Room {
 
     /**
      *
-     * @return
+     * @return x
      */
     public int getDimensionX() {
         return dimensionX;
     }
 
+    /**
+     *
+     * @return y
+     */
     public int getDimensionY() {
         return dimensionY;
     }
 
-    public static BufferedImage getDoorImgTop() {
-        return doorImgTop;
-    }
-
-    public static BufferedImage getDoorImgRight() {
-        return doorImgRight;
-    }
-
-    public static BufferedImage getDoorImgBot() {
-        return doorImgBot;
-    }
-
-    public static BufferedImage getDoorImgLeft() {
-        return doorImgLeft;
-    }
 
     public static int[] getBotDoorPos() {
         return botDoorPos;
@@ -92,10 +90,6 @@ public class Room {
 
     public boolean[] getDoors(){
         return new boolean[] {doorTop,doorRight,doorBot,doorLeft};
-    }
-
-    public BufferedImage[] getBackgroundImages() {
-        return backgroundImages;
     }
 
     public static int getWallWidth() {
